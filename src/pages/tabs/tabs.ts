eb9902706f4729f05cc509e8bbd6f7d7df70ad04
Virve Liimatta <virve.liimatta@gmail.com>
2017-11-12 21:02:51 +0200

    Initial Commit

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { AddPage } from '../add/add';
+import { MoviesPage } from '../movies/movies';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = AddPage;
+  tab2Root = MoviesPage;
+  tab3Root = AboutPage;
 
   constructor() {
 
